e577b3bc48b4249887f268aaa80cb025387cb10b
EpicAryan <adaryan0811@gmail.com>
2025-06-21 16:37:37 +0530

    feat: add social login functionality

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,6 @@
 import { SignInView } from "./auth/sign-in-view";
 import { SignUpView } from "./auth/sign-up-view";
 import { PendingLoader } from "./loaders/pendingLoader";
+import { HomeView } from "./home/home-view";
 
-export { SignInView, SignUpView, PendingLoader}
+export { SignInView, SignUpView, PendingLoader, HomeView}
