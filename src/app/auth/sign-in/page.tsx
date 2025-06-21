93fcda7fd6fe48809566cac51e5bbc8a8e23ba6c
EpicAryan <adaryan0811@gmail.com>
2025-06-21 13:26:43 +0530

    feat: implemented auth ui

---
+++ src/app/auth/sign-in/page.tsx
@@ -0,0 +1,10 @@
+import { SignInView } from '@/components'
+import React from 'react'
+
+const SignInPage = () => {
+  return (
+    <SignInView/>
+  )
+}
+
+export default SignInPage
---
+++ src/components/index.ts
@@ -0,0 +1,5 @@
+import { SignInView } from "./auth/sign-in-view";
+import { SignUpView } from "./auth/sign-up-view";
+import { PendingLoader } from "./loaders/pendingLoader";
+
+export { SignInView, SignUpView, PendingLoader}
