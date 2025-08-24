828224df1d00df920ffe506723ab461b17ea6b18
EpicAryan <adaryan0811@gmail.com>
2025-08-24 22:11:36 +0530

    feat: implement video call page

---
+++ src/app/call/[meetingId]/page.tsx
@@ -0,0 +1,39 @@
+import { CallView } from "@/components/videocall/call-view";
+import { auth } from "@/lib/auth";
+import { getQueryClient, trpc } from "@/trpc/server";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+
+
+interface Props {
+    params: Promise<{
+        meetingId: string;
+    }>;
+}
+
+const VideoCallPage = async ({params}: Props) => {
+    const session = await auth.api.getSession({
+        headers: await headers(),
+    })
+
+    if(!session){
+        redirect("/sign-in");
+    }
+
+    const { meetingId } = await params;
+
+    const queryClient = getQueryClient();
+    void queryClient.prefetchQuery(
+        trpc.meetings.getOne.queryOptions({ id: meetingId }),
+    );
+
+    return (
+        <HydrationBoundary state={dehydrate(queryClient)}>
+            <CallView meetingId={meetingId}/>
+        </HydrationBoundary>
+    )
+}
+
+export default VideoCallPage
